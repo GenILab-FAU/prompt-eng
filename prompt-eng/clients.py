1f3fb5a10153023c6981e2f776df9d1ea324563c
Daniel Bennett <dbenne11@fau.edu>
2025-02-09 15:44:32 -0500

    added ollama client

--- prompt-eng/clients.py
+++ prompt-eng/clients.py
@@ -6,6 +6,7 @@ from models import AIModel
 from config import config_factory
 import random
 import logging
+import json
 
 logger = logging.getLogger(__name__)
 
@@ -19,8 +20,16 @@ class ChatbotClient(ABC):
     def chat(self, message: str, model: AIModel, options: Dict[str, Any]) -> Tuple[int, str]:
         pass
 
-    def set_system_prompt(self, prompt: str):
-        pass
+    def set_system_prompt(self, prompt: str) -> None:
+        if prompt:
+            self._system_prompt = prompt
+
+    # this format is currently the same for both APIS, consider overriding if your client has different requirements.
+    def _generate_system_message(self) -> Dict[str,str]:
+        if self._system_prompt:
+            return {"role": "system", "content": self._system_prompt}
+        else:
+            return {}
 
 
 # instead of trying to make all the decisions in the client, we can use a factory to create the appropriate client based on the configuration.
@@ -33,9 +42,8 @@ class ChatbotClientFactory():
         # ollama client for now.
         if host == "chat.hpc.fau.edu":
             return OpenWebUIClient(host=host, bearer=config["bearer"])
-        # TODO : Implement Ollama client.
         else:
-            raise ValueError(f"Unknown host: {host}")
+            return OllamaClient(host=host)
 
         
 class OpenWebUIClient(ChatbotClient):
@@ -55,17 +63,6 @@ class OpenWebUIClient(ChatbotClient):
                 implemented_options[key] = value
         
         return implemented_options
-    
-    def set_system_prompt(self, prompt: str) -> None:
-        if prompt:
-            self._system_prompt = prompt
-
-    def _generate_system_message(self) -> Dict[str,str]:
-        if self._system_prompt:
-            return {"role": "system", "content": self._system_prompt}
-        else:
-            return {}
-
 
     def _get_models(self) -> Dict[Any, Any]:
         """Get the list of available models from the server."""
@@ -117,7 +114,7 @@ class OpenWebUIClient(ChatbotClient):
             "model": model_name,
             "messages": [
                 {"role": "user",
-                 "content": message},
+                 "content": message}
             ]
         }
         
@@ -127,13 +124,79 @@ class OpenWebUIClient(ChatbotClient):
         response = requests.post(url, json=post_body, headers=headers)
         response.raise_for_status()
         return response.json()
+    
+class OllamaClient(ChatbotClient):
+    def __init__(self, host: str):
+        self._host = host
+        self._system_prompt = ""
+
+    def chat(self, message: str, model: AIModel, options: Dict[str, Any] = {}) -> Tuple[int, str]:
+        """Send a chat request to the API and get the response."""
+        start_time = datetime.now(timezone.utc)
+        response = self._chat(message, model.name, options)
+        end_time = datetime.now(timezone.utc)
+        time_taken_in_ms = (end_time - start_time).total_seconds() * 1000
+        return time_taken_in_ms, response["message"]["content"]
+
+
+    def _chat(self, message: str, model_name: str, options: Dict[str, Any] = {}) -> Dict[str,str]:
+        """Send a chat request to the API and get the response."""
+        url = f"http://{self._host}/api/chat"
+        post_body = {
+            "model": model_name,
+            "messages": [
+                {
+                "role": "user",
+                "content": message
+                }
+            ],
+            "stream": False
+        }
+        post_body.update(self._parse_options(options))
+        if self._system_prompt:
+            post_body["messages"] = [self._generate_system_message()] + post_body["messages"]
+        breakpoint()
+        response = requests.post(url, json=post_body, timeout=600)
+        response.raise_for_status()
+        return response.json()
+
+
+    def _parse_options(self, options: Dict[str, Any]) -> None:
+        parsed_options={}
+        nested_options = {}
+        for key, value in options.items():
+            if key in ["max_tokens"]:
+                nested_options["num_predict"] = value
+            if key in ["temperature"]:
+                nested_options[key] = value
+        if nested_options:
+            parsed_options["options"] = nested_options
+        return parsed_options
+   
+    def get_models(self) -> List[AIModel]:  
+        """Get a list of available models."""
+        data = self._get_models()
+        return [AIModel(name=model["name"],parameter_size=model["details"]["parameter_size"]) for model in data["models"]]
+
+    def _get_models(self):
+        """Get a list of available models."""
+        url = f"http://{self._host}/api/tags"
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.json()
+    
+    def set_system_prompt(self, prompt):
+        if prompt:
+            self._system_prompt = prompt
+        
+
 
 # this is just for ease of use, bootstraps the config and instantiates a model and a client
 def bootstrap_client_and_model(model: str) -> Tuple[ChatbotClient, AIModel]:
     config = config_factory()
     client = ChatbotClientFactory.create_client(config)
     models = client.get_models()
-    fallback_model = random.choice(models)
+    fallback_model = _get_smallest_model(models)
     picked_model = None
     for modl in models:
         if model == modl.name:
@@ -144,3 +207,13 @@ def bootstrap_client_and_model(model: str) -> Tuple[ChatbotClient, AIModel]:
         picked_model = fallback_model
     return client, picked_model
 
+def _get_smallest_model(models: List[AIModel]):
+    smallest = 9999999
+    chosen_model = None
+    for model in models:
+        size = float(model.parameter_size.strip("B"))
+        if size < smallest:
+            smallest = size
+            chosen_model = model
+    
+    return chosen_model
